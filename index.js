94d2f41dcbc5f37840734a40c1fc850f0bb9da15
Elelyatar <elelyatar@gmail.com>
2021-09-18 21:11:38 +0200

    v1.0.1.

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 import SecurityKeyboardInput from "./src/component/keyboardMethods"
 import ParcelKeyboardView from "./src/component/ParcelKeyboardView"
 
+// TODO index.d.ts
+// Pusblish with:
+// npm publish --access public
+
 export { SecurityKeyboardInput, ParcelKeyboardView }
