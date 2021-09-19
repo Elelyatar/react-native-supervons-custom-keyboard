b0ad7a0d5271568652e7c39ef016194b0d5f2a4e
Elelyatar <elelyatar@gmail.com>
2021-09-19 23:03:43 +0200

    index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,4 @@
+import SecurityKeyboardInput from "./src/component/keyboardMethods"
+import ParcelKeyboardView from "./src/component/ParcelKeyboardView"
+
+export { SecurityKeyboardInput, ParcelKeyboardView }
